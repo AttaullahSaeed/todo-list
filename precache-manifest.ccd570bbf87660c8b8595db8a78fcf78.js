5b1a8438590315d9fe1c556394e265d600d42d08
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-08-26 14:45:30 +0500

    Updates

---
+++ precache-manifest.ccd570bbf87660c8b8595db8a78fcf78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bf2d838358c648b8a823d6792685fb",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "963ed4c598132a310a90",
+    "url": "/todo-list/static/css/main.749fa775.chunk.css"
+  },
+  {
+    "revision": "37d1e485874b1ca37cee",
+    "url": "/todo-list/static/js/2.06b6c45e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.06b6c45e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963ed4c598132a310a90",
+    "url": "/todo-list/static/js/main.e8c74699.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
